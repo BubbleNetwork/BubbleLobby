2ee6316c8dc1eeece37bcde340f336200248e5fd
ExtendObject <clipcojnh@gmail.com>
2016-03-13 23:07:37 +0000

    Tab names and creature spawning

--- src/main/java/com/thebubblenetwork/bubblelobby/LobbyListener.java
+++ src/main/java/com/thebubblenetwork/bubblelobby/LobbyListener.java
@@ -1,6 +1,9 @@
 package com.thebubblenetwork.bubblelobby;
 
+import com.thebubblenetwork.api.framework.BukkitBubblePlayer;
+import com.thebubblenetwork.api.framework.plugin.BubbleRunnable;
 import com.thebubblenetwork.api.framework.util.mc.items.ItemStackBuilder;
+import com.thebubblenetwork.api.global.player.BubblePlayer;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,6 +16,10 @@ import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scoreboard.Team;
+
+import java.util.Collection;
+import java.util.UUID;
 
 /**
  * Copyright Statement
@@ -33,12 +40,12 @@ public class LobbyListener implements Listener {
 
     protected static final int COMPASSSLOT = 0;
 
-    private ItemStackBuilder compass = new ItemStackBuilder(Material.COMPASS).withAmount(1).withName(ChatColor.AQUA + "Compass").withLore(ChatColor.GRAY + "Right-click to open up the server-menu!", ChatColor.GRAY + "You can access any gamemode");
+    private ItemStackBuilder compass = new ItemStackBuilder(Material.COMPASS).withAmount(1).withName(ChatColor.AQUA + "Compass").withLore(ChatColor.GRAY + "Right-click to open up the server-menu!");
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-        Player p = e.getPlayer();
-        LobbyScoreboard.createBoard(p);
+        final Player p = e.getPlayer();
+        final LobbyScoreboard scoreboard = LobbyScoreboard.createBoard(p);
         p.setGameMode(GameMode.ADVENTURE);
         p.setLevel(0);
         p.setFoodLevel(20);
@@ -46,24 +53,46 @@ public class LobbyListener implements Listener {
         p.getInventory().setArmorContents(new ItemStack[4]);
         p.getInventory().setContents(generateInventory());
         p.teleport(BubbleLobby.getInstance().getSpawn().toLocation(BubbleLobby.getInstance().getW()));
+        final UUID u = p.getUniqueId();
+        new Thread(){
+            @Override
+            public void run() {
+                if(p.isOnline()) {
+                    BukkitBubblePlayer player = BukkitBubblePlayer.getObject(u);
+                    if (player != null) {
+                        for(LobbyScoreboard other:LobbyScoreboard.getBoards()){
+                            other.applyRank(player.getRank(),p);
+                        }
+                    }
+                    for(BubblePlayer other:BukkitBubblePlayer.getPlayerObjectMap().values()){
+                        scoreboard.applyRank(other.getRank(),(Player)other.getPlayer());
+                    }
+                }
+            }
+        }.start();
     }
 
-    @EventHandler
-    public void onCreatureSpawn(CreatureSpawnEvent e){
-        switch (e.getSpawnReason()){
-            case NATURAL:
-            case REINFORCEMENTS:
-            case NETHER_PORTAL:
-            case SPAWNER:
-            case LIGHTNING:
-            case INFECTION:
-                e.setCancelled(true);
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onCreatureSpawn(CreatureSpawnEvent e) {
+        if(e.getSpawnReason() != CreatureSpawnEvent.SpawnReason.CUSTOM){
+            e.setCancelled(true);
         }
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent e) {
-        LobbyScoreboard.removeBoard(e.getPlayer().getUniqueId());
+        final Player p = e.getPlayer();
+        LobbyScoreboard.removeBoard(p.getUniqueId());
+        new Thread(){
+            @Override
+            public void run(){
+                for(LobbyScoreboard other:LobbyScoreboard.getBoards()){
+                    for(Team t:other.getObject().getBoard().getTeams()){
+                        t.removePlayer(p);
+                    }
+                }
+            }
+        }.start();
     }
 
     public ItemStack[] generateInventory() {
